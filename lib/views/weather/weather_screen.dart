2f32b92f585f0d9bfe76b5d16238cf0848a9d48e
Phạm Gia Mỹ <54151865+erasiii@users.noreply.github.com>
2021-06-17 01:26:40 +0700

    Update weather_screen.dart

--- lib/views/weather/weather_screen.dart
+++ lib/views/weather/weather_screen.dart
@@ -7,38 +7,24 @@ import 'package:flutter_weather_mn/views/weather/weather_error.dart';
 import 'package:flutter_weather_mn/views/weather/weather_view.dart';
 
 class LocationScreen extends StatelessWidget {
-  final int accent;
-  final bool loading;
   final Location location;
   final Animation<double> controller;
 
   LocationScreen({
     this.location,
-    this.loading,
-    this.accent = 0,
     this.controller,
   });
 
   @override
   Widget build(BuildContext context) {
     Weather weather = location.getWeather();
-    if (loading) {
-      return Center(
-        child: SpinKitPulse(
-          size: 100,
-          color: GradientValues().gradients[accent].gradient[0],
-        ),
-      );
-    } else {
       if (weather == null) {
         return ErrorView();
       } else {
         return LocationView(
           controller: controller,
           weather: weather,
-          accent: accent,
         );
       }
     }
   }
-}
